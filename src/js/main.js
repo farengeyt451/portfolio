9a158d1c9788abdda7eeaab3175e9cb9f734fcce
Alexander <strannik11.92@gmail.com>
2017-08-31 20:46:08 +0300

    Add header text for small screens

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,14 @@
 
 $( document ).ready(function() {
 	$(".logo").fadeIn(2500);
+	pullHeaderImg();
 });
+
+function pullHeaderImg() {
+	var vWidth = $( window ).width();
+	if (vWidth < 768) {
+		$(".logo").append("<\img src = \"img/header-text-sm-screens.svg\" alt=\"logo &amp; initials\">");
+	} else {
+		$(".logo").append("<\img src = \"img/header-text.svg\" alt=\"logo &amp; initials\">");
+	}
+}
