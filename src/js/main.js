f9ccc178d71f64aee69846613595c9f5e5d13fbf
Alexander <strannik11.92@gmail.com>
2017-09-03 16:41:20 +0300

    Add particle js anim to header && cleaning project

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,2 @@
-
-$( document ).ready(function() {
-	$(".logo").fadeIn(2500);
-	pullHeaderImg();
-});
-
-function pullHeaderImg() {
-	var vWidth = $( window ).width();
-	if (vWidth < 768) {
-		$(".logo").append("<\img src = \"img/header-text-sm-screens.svg\" alt=\"logo &amp; initials\">");
-	} else {
-		$(".logo").append("<\img src = \"img/header-text.svg\" alt=\"logo &amp; initials\">");
-	}
-}
+//= partials/tabs.js
+//= partials/particle.js
